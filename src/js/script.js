a88fed64f272b6fc3c7969152ee9ea2920105c84
mangupli <elizaveta.mangupli@gmail.com>
2022-09-11 23:25:45 +0300

    draft of a catalog + footer with a map

--- src/js/script.js
+++ src/js/script.js
@@ -15,8 +15,29 @@ $(document).ready(function(){
                 arrow: false
               }
             }
-        ]
-        
+        ]      
   });
+
+     
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+
+    function toggleCatalogCards(className){  
+      $(className).each( function(i){
+        $(this).on('click', function(e){
+          e.preventDefault();
+          $('.catalog-item__main-info' ).eq(i).toggleClass('catalog-item__main-info_active');
+          $('.catalog-item__secondary-info').eq(i).toggleClass('catalog-item__secondary-info_active');
+        });
+      });
+    }
+
+    toggleCatalogCards('.catalog-item__link_details');
+    toggleCatalogCards('.catalog-item__link_back');
+
 });
           
